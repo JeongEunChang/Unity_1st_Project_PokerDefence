6d1d88c4e99a84c9a8e4c2c51e5bc9f8170cd52a
JeongEunChang <101631103+JeongEunChang@users.noreply.github.com>
2022-05-25 02:46:51 +0900

    2022.05.25
    타워 공격기능 일부 구현

--- PokerDefence/Assets/Script/CreateTower.cs
+++ PokerDefence/Assets/Script/CreateTower.cs
@@ -8,17 +8,14 @@ public class CreateTower : MonoBehaviour
     public enum TowerBuildKind { None, Laser, MachineGun, Rocket, Pylon };
     public static TowerBuildKind kind = TowerBuildKind.None;
 
-    public GameObject SelectLocate = null;
+    public static GameObject SelectLocate = null;
     public GameObject Tower;
     public GameObject[] TowerHouse;
 
     // Start is called before the first frame update
     void Start()
     {
-
-
-
-
+        
     }
 
     // Update is called once per frame
@@ -38,21 +35,89 @@ public class CreateTower : MonoBehaviour
         {
             if (UIScript.BuildCheck == true)
             {
+                GameObject Check = GameObject.Find("CanCreateTower");
+
                 switch (kind)
                 {
                     case TowerBuildKind.Laser:
-                        Tower = Instantiate(TowerHouse[0]);
-                        Tower.transform.position = SelectLocate.transform.position;
-                        Tower.transform.rotation = SelectLocate.transform.rotation;
+                        if (ResourceScript.MinusMoney(50))
+                        {
+                            Tower = Instantiate(TowerHouse[0]);
+                            Tower.transform.position = SelectLocate.transform.position;
+                            Tower.transform.rotation = SelectLocate.transform.rotation;
+                        }
+
+                        else
+                            print("돈이 부족해요ㅠㅠ");
 
                         kind = TowerBuildKind.None;
 
-                        GameObject Check = GameObject.Find("CanCreateTower");
+                        //GameObject Check = GameObject.Find("CanCreateTower");
+                        Check.SetActive(false);
+                        UIScript.BuildCheck = false;
+                        Destroy(SelectLocate);
+                        break;
+
+                    case TowerBuildKind.MachineGun:
+                        if (ResourceScript.MinusMoney(50))
+                        {
+                            Tower = Instantiate(TowerHouse[1]);
+                            Tower.transform.position = SelectLocate.transform.position;
+                            Tower.transform.rotation = SelectLocate.transform.rotation;
+                        }
+
+                        else
+                            print("돈이 부족해요ㅠㅠ");
+
+                        kind = TowerBuildKind.None;
+
+                        //GameObject Check = GameObject.Find("CanCreateTower");
+                        Check.SetActive(false);
+                        UIScript.BuildCheck = false;
+                        Destroy(SelectLocate);
+                        break;
+
+                    case TowerBuildKind.Rocket:
+                        if (ResourceScript.MinusMoney(80))
+                        {
+                            Tower = Instantiate(TowerHouse[2]);
+                            Tower.transform.position = SelectLocate.transform.position;
+                            Tower.transform.rotation = SelectLocate.transform.rotation;
+                        }
+
+                        else
+                            print("돈이 부족해요ㅠㅠ");
+
+                        kind = TowerBuildKind.None;
+
+                        //GameObject Check = GameObject.Find("CanCreateTower");
+                        Check.SetActive(false);
+                        UIScript.BuildCheck = false;
+                        Destroy(SelectLocate);
+                        break;
+
+                    case TowerBuildKind.Pylon:
+                        if (ResourceScript.MinusMoney(50))
+                        {
+                            Tower = Instantiate(TowerHouse[3]);
+                            Tower.transform.position = SelectLocate.transform.position;
+                            Tower.transform.rotation = SelectLocate.transform.rotation;
+                        }
+
+                        else
+                            print("돈이 부족해요ㅠㅠ");
+
+                        kind = TowerBuildKind.None;
+
+                        //GameObject Check = GameObject.Find("CanCreateTower");
                         Check.SetActive(false); ;
                         UIScript.BuildCheck = false;
-
                         Destroy(SelectLocate);
+                        break;
 
+
+                    case TowerBuildKind.None:
+                        SelectLocate = null;
                         break;
                 }
 
