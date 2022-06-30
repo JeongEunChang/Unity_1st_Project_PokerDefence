3eae73f75ac956f753ed34b4d84c02a49cc66d5f
JeongEunChang <101631103+JeongEunChang@users.noreply.github.com>
2022-06-30 16:55:48 +0900

    2022.06.30
    레이저 타워 파이클 시스템 일부 적용

--- PokerDefence/Assets/Script/HPScript.cs
+++ PokerDefence/Assets/Script/HPScript.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class HPScript : MonoBehaviour
 {
     public int HP;
+    public ParticleSystem LaserParticle;
+    public int count = 0;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        LaserParticle = GameObject.Find("Desktop").GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -21,6 +23,16 @@ public class HPScript : MonoBehaviour
             Destroy(gameObject);
             Regen.DiedMonster++;
         }
+    }
+
+    public void OnParticleCollision(GameObject other)
+    {
+        //Debug.Log("Ãæµ¹");
+        //if (LaserParticle.isPlaying) LaserParticle.Stop();
+        HPScript hp = gameObject.GetComponent<HPScript>();
+        hp.DamagedHP(60);
+        count++;
+        LaserParticle.Stop();
     }
 
     public int GetHP()
--- PokerDefence/Assets/Script/LaserAttack.cs
+++ PokerDefence/Assets/Script/LaserAttack.cs
@@ -9,20 +9,40 @@ public class LaserAttack : MonoBehaviour
     public GameObject Hit; // 생성한 레이저 오브젝트를 담아두는 오브젝트 변수
     public GameObject obj; // 레이저 프리펩을 받아오는 오브젝트 변수
     public GameObject Fire;
+    public GameObject tmp;
+    public ParticleSystem LaserParticle;
     public bool Delay = false;
     float timer = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        LaserParticle = GameObject.Find("Desktop").GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Delay == true)
+        {
+            timer += Time.deltaTime;
 
+            if (timer > 1.0f)
+            {
+                timer = 0f;
+                Delay = false;
+            }
+        }
     }
+
+    //private void OnParticleCollision(GameObject other)
+    //{
+    //    Debug.Log("충돌");
+    //    //if (LaserParticle.isPlaying) LaserParticle.Stop();
+    //    HPScript hp = other.GetComponent<HPScript>();
+    //    hp.DamagedHP(60);
+    //    LaserParticle.Stop();
+    //}
 
     private void OnTriggerStay(Collider other) // 공격범위로 들어오는 적을 인식해서, 가장 가까운 적으로 타겟으로 변경
     {
@@ -30,19 +50,25 @@ public class LaserAttack : MonoBehaviour
         {
             if (NowEnemy == null)
             {
+                Debug.Log(LaserParticle.time);
                 NowEnemy = other.gameObject;
-                Hit = Instantiate(obj);
+                LaserParticle.Play();
+                //LaserParticle.Stop();
+                //LaserParticle.Stop();
+                Debug.Log(LaserParticle.time);
+                //Hit = Instantiate(obj);
                 //Debug.Log("0");
                 //Debug.Log("1");
-                Hit.transform.position = Fire.transform.position;
+                //Hit.transform.position = Fire.transform.position;
                 //Debug.Log("2");
                 //Debug.Log(Fire);
                 //Hit.transform.rotation = new Quaternion(Fire.transform.rotation.x, Fire.transform.rotation.y, 180, 0);
-                Hit.GetComponent<Missile>().Aim = NowEnemy;
+                //Hit.GetComponent<Missile>().Aim = NowEnemy;
             }
 
             else if (NowEnemy != null)
             {
+                Debug.Log("카운팅");
                 float Now = (NowEnemy.transform.position - gameObject.transform.position).magnitude;
                 float New = (other.transform.position - gameObject.transform.position).magnitude;
 
@@ -51,24 +77,25 @@ public class LaserAttack : MonoBehaviour
                     NowEnemy = other.gameObject;
                 }
 
-                Hit = Instantiate(obj);
-                Hit.transform.position = Fire.transform.position;
-                //Hit.transform.rotation = new Quaternion(Fire.transform.rotation.x, Fire.transform.rotation.y, Fire.transform.rotation.z, 0);
-                Hit.GetComponent<Missile>().Aim = NowEnemy;
+                LaserParticle.Play();
+                //LaserParticle.Stop();
+                //Hit = Instantiate(obj);
+                //Hit.transform.position = Fire.transform.position;
+                //Hit.GetComponent<Missile>().Aim = NowEnemy;
             }
 
             Delay = true;
         }
 
-        else if (Delay)
-        {
-            timer += Time.deltaTime;
+        //else if (Delay)
+        //{
+        //    timer += Time.deltaTime;
             
-            if (timer > 1.0f)
-            {
-                timer = 0f;
-                Delay = false;
-            }
-        }
+        //    if (timer > 1.0f)
+        //    {
+        //        timer = 0f;
+        //        Delay = false;
+        //    }
+        //}
     }
 }
